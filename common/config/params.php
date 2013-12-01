1f04b7bb77406a8fa3ba2593510d50825a66edf0
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2013-12-01 14:57:29 +0100

    Use correct path alias for rbac file

--- common/config/params.php
+++ common/config/params.php
@@ -28,9 +28,7 @@ return [
 	],
 	'components.auth' => [
 		'class' => 'common\components\RBACPhpManager',
-		// @Todo: Fix after the bugfix for issue https://github.com/yiisoft/yii2/issues/1356 in Yii is done
-		//'authFile' => '@common/data/rbac.php',
-		'authFile' => realpath(__DIR__ . '/../').'/data/rbac.php',
+		'authFile' => '@common/data/rbac.php',
 		'defaultRoles' => ['guest'],
 	],
 ];
