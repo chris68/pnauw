a218a6894f0f30e0fc3546692cda24ff68c52ad2
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2013-12-01 14:58:21 +0100

    Use SliderAsset.php file in the same namespace (see https://github.com/yiisoft/yii2/issues/1377)

--- frontend/widgets/SliderInput.php
+++ frontend/widgets/SliderInput.php
@@ -48,7 +48,9 @@ class SliderInput extends InputWidget
 			}';
 		}
 
-		$this->registerWidget('slider', \yii\jui\SliderAsset::className());
+		// $this->registerWidget('slider', \yii\jui\SliderAsset::className());
+		// See https://github.com/yiisoft/yii2/issues/1377
+		$this->registerWidget('slider', SliderAsset::className());
 		$this->getView()->registerJs('$("#'.$inputId.'").val($("#'.$this->id.'").slider("value"));');
 	}
 }
