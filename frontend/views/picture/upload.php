fa78e6d6158efe4864ffcff9745265f4587501ad
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2013-12-24 11:50:47 +0100

    Small rephrase of the text

--- frontend/views/picture/upload.php
+++ frontend/views/picture/upload.php
@@ -27,7 +27,7 @@ $this->params['breadcrumbs'][] = 'Hochladen';
 	<div class="help-block">
 		<p>Hier können Sie auf einen Rutsch <b>maximal ca. 50 Dateien</b> auswählen, die Sie hochladen wollen. Es werden nur <b>JPG-Bilder</b> mit <b>maximal ca. 1 MB</b> pro Bild akzeptiert. Wenn Ihre Bilder im Original größer sind, dann verkleinern Sie diese bitte vorher.</p>
 		<p>Die Bilder sollten am besten dem <b>Format 3:4</b> entsprechen. Andernfalls werdem Sie eventuell verzerrt dargestellt.</p>
-		<p>Die Bilder müssen ein <b>Exif-Aufnahmedatum</b> (Exif-Tag <i>DateTimeOriginal</i>) haben, sonst werden die Bilder abgelehnt.</p>
+		<p>Die Bilder müssen ein <b>Exif-Aufnahmedatum</b> (Exif-Tag <i>DateTimeOriginal</i>) haben, sonst werden die Bilder auf den 1.1.1970 datiert.</p>
 		<p>Es ist hilfreich, wenn die Bilder bereits <b>Exif-Geo/GPS-Informationen</b> (Exif-Tags <i>GPSLatitude</i>, <i>GPSLatitudeRef</i>, <i>GPSLongitude</i>, <i>GPSLongitudeRef</i>) beinhalten. Aber man kann die Position der Bilder auch später manuell nachdokumentieren.</p>
 		<p>Die Bilder werden nicht gleich freigeschaltet, sondern müssen erst vom Ihnen <b>veröffentlicht</b> und teilweise dann auch noch von einem Moderator <b>freigegeben</b> werden.</p>
 	</div>
