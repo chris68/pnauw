423f854f9585261f6829a701bcd61614e791cd4b
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2013-12-29 17:00:47 +0100

    Rephrase of text

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -13,8 +13,11 @@ $this->params['breadcrumbs'][] = $this->title;
 		Problem: Mißbräuchliches Parken auf Gehwegen, Radwegen oder in verkehrsberuhigten Zonen
 	</h4>
 	<p>
-		Das Problem des Gehwegparkens ist nach wie vor akut und leider wird es von offiziellen Stellen eher nicht ausreichend verfolgt obwohl es gemäß <a href="http://dejure.org/gesetze/StVO/12.html">StVO §12</a> in der Regel verboten ist. 
-		Was viele Leute aber gar nicht wissen, sondern wie selbstverständlich lieber auf dem Gehweg parken als korrekt auf der Straße.
+		Das Problem des Gehwegparkens ist nach wie vor akut und leider wird es von offiziellen Stellen eher nicht ausreichend verfolgt
+	</p>
+	<p> 
+		Obwohl es gemäß <a href="http://dejure.org/gesetze/StVO/12.html">StVO §12</a> in der Regel verboten ist. 
+		Was aber viele Leute gar nicht wissen, sondern wie selbstverständlich lieber auf dem Gehweg parken als korrekt auf der Straße.
 	</p>
 	<p>
 		Doch auf dieser Seite soll gar nicht versucht werden, dies genauer zu beleuchten, sondern nur auf andere relevante Seiten zu dem Thema verwiesen werden
@@ -36,14 +39,23 @@ $this->params['breadcrumbs'][] = $this->title;
 		Da Politik und die offiziellen Stellen das Problem nicht gelöst bekommen, will diese Plattform betroffenen Bürgern die Möglichkeit geben, sich zu wehren und den Finger auf die Wunde zu legen.
 	</p>
 	<p>
-		In erster Linie durch Dokumentation der Mißstände und der leichten Möglichkeit, dann wieder und immer wieder bei den offiziellen Stellen nachzuhaken, warum der gut und öffentliche dokumentierte Mißstand nicht beseitigt wird.
+		In erster Linie durch <b>Dokumentation der Mißstände</b> und der leichten Möglichkeit, dann wieder und immer wieder bei den offiziellen Stellen nachzuhaken, warum der gut und öffentliche dokumentierte Mißstand nicht beseitigt wird.
 	</p>
 	<p>
-		Aber in zweiter Linie soll es allen Betroffenen auch die Möglichkeit geben, das Problem durch Privatanzeigen anzugehen. Privatanzeigen sind eine Möglichkeit, die der Gesetzgeber ausdrücklich vorsieht, und in der Regel wird dadurch das Problem schnell gelöst. 
+		Aber in zweiter Linie soll es allen Betroffenen auch die Möglichkeit geben, das Problem durch <b>Privatanzeigen</b> anzugehen. Privatanzeigen sind eine Möglichkeit, die der Gesetzgeber ausdrücklich vorsieht, und in der Regel wird dadurch das Problem schnell gelöst. 
 	</p>
 	<p>
 		Jeder kann dann für sich selbst entscheiden, welche der bereitgestellten Mittel er einsetzen möchte.
 	</p>
-	
-	
+	<h4>
+		Die Plattform "Parke nicht auf unseren Wegen"
+	</h4>
+	<p>
+		Die Plattform ist eine Webapplikation, die voll auf HTML5 und den damit verbundenen Möglichkeiten setzt. Die Webseiten sind hierbei 
+		speziell für den Einsatz auf mobilen Geräten optimiert. Ältere Browser (vor allem nicht ältere Internet Exlorer!) werden als Konsequenz 
+		nicht unterstützt und es wird ausdrücklich empfohlen, einen aktuellen und HTML5-kompatiblen Browser einzusetzen.
+	</p>
+	<p>
+		Sollte es Probleme mit <b>aktuellen</b> Browsern geben, dann melden Sie diese bitte über die Kontaktseite.
+	</p>
 </div>
