5661bc5787a59c488f3398f433dd6c4521b4502e
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2014-01-07 21:23:55 +0100

    Changed the order of the javascript initialization; also give the map a default center and zoom (otherwise on slow connections we sometimes do not have a map at all!!!)

--- frontend/views/picture/assets/pnauw.picture.heatmap.js
+++ frontend/views/picture/assets/pnauw.picture.heatmap.js
@@ -1,13 +1,25 @@
 $(function() {
 	var map;
 	var mapOptions = {
-	  mapTypeId: google.maps.MapTypeId.ROADMAP
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		center: new google.maps.LatLng(0, 0),
+		zoom: 10,
 	};
 
 	map = new google.maps.Map($('#picture-heatmap-map-canvas').get(0), mapOptions);
 
-	var input = (document.getElementById('picture-heatmap-search-address'));
-	var searchBox = new google.maps.places.SearchBox(input);
+	if ($('#search-map-bounds').val() != '') {
+		// Restore previous bounds if set
+		var corners = $('#search-map-bounds').val().split(',',4);
+		var sw = new google.maps.LatLng(corners[0],corners[1]);
+		var ne = new google.maps.LatLng(corners[2],corners[3]);
+		bounds = new google.maps.LatLngBounds(sw,ne);
+		// console.debug('Bounds restored - Target ('+bounds.toUrlValue()+')');
+		map.fitBounds(bounds);
+		// Google zooms just one zoom level lower - so we need to zoom in again!
+		map.setZoom(map.getZoom()+1);
+		// console.debug('Bounds restored - Actual ('+map.getBounds().toUrlValue()+')');
+	}
 
 	$('#picture-heatmap-search-address').on( 'keydown', function( e ) {
 		if ( e.keyCode === 13 ) {
@@ -16,71 +28,6 @@ $(function() {
 		}
 	});
 
-	google.maps.event.addListener(searchBox, 'places_changed', function() {
-		var places = searchBox.getPlaces();
-
-		var bounds = new google.maps.LatLngBounds();
-		for (var i = 0, place; place = places[i]; i++) {
-		  bounds.extend(place.geometry.location);
-		  if (place.geometry.viewport !== undefined) {
-			bounds.union(place.geometry.viewport);
-		  }
-		}
-
-		map.fitBounds(bounds);
-		map.setZoom(map.getZoom()+2);
-		if (map.getZoom() > 17) {
-			map.setZoom(17);
-		}
-	});
-
-	// Idle is better than bounds_changed since fires only once at the end
-	google.maps.event.addListener(map, 'idle', function() {
-		var bounds = map.getBounds();
-		if (bounds) {
-			// console.debug('Bounds updated ('+bounds.toUrlValue()+')');
-			$('#search-map-bounds').val(bounds.toUrlValue());
-			searchBox.setBounds(bounds);
-		}
-	});
-
-	$.getJSON( heatmapSource, function( data ) {
-		var heatmap;
-		var points = [];
-		var bounds = new google.maps.LatLngBounds();
-
-		$.each( data, function( key, val ) {
-			var point = new google.maps.LatLng(val.location.lat, val.location.lng);
-			// Construct a weighted location
-			points.push({location:point, weight:val.severity+1});
-			bounds.extend(point);
-		});
-		
-		if ($('#search-map-bounds').val() == '') {
-			map.fitBounds(bounds);
-			// console.debug('Bounds calculated ('+bounds.toUrlValue()+')');
-		} else {
-			var corners = $('#search-map-bounds').val().split(',',4);
-			var sw = new google.maps.LatLng(corners[0],corners[1]);
-			var ne = new google.maps.LatLng(corners[2],corners[3]);
-			bounds = new google.maps.LatLngBounds(sw,ne);
-			// console.debug('Bounds restored - Target ('+bounds.toUrlValue()+')');
-			map.fitBounds(bounds);
-			// Google zooms just one zoom level lower - so we need to zoom in again!
-			map.setZoom(map.getZoom()+1);
-			// console.debug('Bounds restored - Actual ('+map.getBounds().toUrlValue()+')');
-		}
-		
-		var pointArray = new google.maps.MVCArray(points);
-
-		heatmap = new google.maps.visualization.HeatmapLayer({
-		   data: pointArray,
-		   map: map,
-		   radius: 10,
-		   opacity: 0.6
-		});
-	});		
-	
 	$("#search-time li").on ( 'click', function( event ) {
 		event.preventDefault();
 		$('#search-time-range').val($(event.target).data('value'));
@@ -151,5 +98,63 @@ $(function() {
 		return false;
 	});
 
+	var input = (document.getElementById('picture-heatmap-search-address'));
+	var searchBox = new google.maps.places.SearchBox(input);
+
+	google.maps.event.addListener(searchBox, 'places_changed', function() {
+		var places = searchBox.getPlaces();
+
+		var bounds = new google.maps.LatLngBounds();
+		for (var i = 0, place; place = places[i]; i++) {
+		  bounds.extend(place.geometry.location);
+		  if (place.geometry.viewport !== undefined) {
+			bounds.union(place.geometry.viewport);
+		  }
+		}
+
+		map.fitBounds(bounds);
+		map.setZoom(map.getZoom()+2);
+		if (map.getZoom() > 17) {
+			map.setZoom(17);
+		}
+	});
+
+	// Idle is better than bounds_changed since it fires only once at the end
+	google.maps.event.addListener(map, 'idle', function() {
+		var bounds = map.getBounds();
+		if (bounds) {
+			// console.debug('Bounds updated ('+bounds.toUrlValue()+')');
+			$('#search-map-bounds').val(bounds.toUrlValue());
+			searchBox.setBounds(bounds);
+		}
+	});
+
+	$.getJSON( heatmapSource, function( data ) {
+		var heatmap;
+		var points = [];
+		var bounds = new google.maps.LatLngBounds();
+
+		$.each( data, function( key, val ) {
+			var point = new google.maps.LatLng(val.location.lat, val.location.lng);
+			// Construct a weighted location
+			points.push({location:point, weight:val.severity+1});
+			bounds.extend(point);
+		});
+		
+		if ($('#search-map-bounds').val() == '') {
+			map.fitBounds(bounds);
+			// console.debug('Bounds calculated ('+bounds.toUrlValue()+')');
+		}
+		
+		var pointArray = new google.maps.MVCArray(points);
+
+		heatmap = new google.maps.visualization.HeatmapLayer({
+		   data: pointArray,
+		   map: map,
+		   radius: 10,
+		   opacity: 0.6
+		});
+	});		
+	
 });
 
