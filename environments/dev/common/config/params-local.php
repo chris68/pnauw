451b5d307abb46424aabb5583012c5f473543816
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2014-01-12 21:04:45 +0100

    Fixed the dbname for dev

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -4,7 +4,7 @@ return [
 		'class' => 'yii\db\Connection',
 		'tablePrefix' => 'tbl_',
 		'charset' => 'utf8',
-		'dsn' => 'pgsql:host=localhost;dbname=pnauw',
+		'dsn' => 'pgsql:host=localhost;dbname=pnauw_dev',
 		'username' => 'mailwitch',
 		// passwort does not matter since we use unix authentication; so just set it to the username
 		'password' => 'mailwitch', 
