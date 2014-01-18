9753b3d6caab4f7be58d75389e5baba1bd3de76d
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2014-01-18 21:52:44 +0100

    Correct usage of form-group

--- frontend/views/picture/_maptab.php
+++ frontend/views/picture/_maptab.php
@@ -9,17 +9,19 @@ use \yii\helpers\Html;
 <div class="row">
 	<div class="col-lg-4">
 		<div class ="form-group">
-			<?php echo Html::label('Kartensuche (via Google Maps)','picture-map-search-address'); ?>
-			<input type="text" id="picture-map-search-address" class="form-control" autocomplete="off" value="<?=$model->loc_formatted_addr?>" placeholder="Kartensuche">
-			<?php echo Html::label('Nächstliegende Adresse (ermittelt via Google Maps)','picture-map-nearest-address'); ?>
-			<p id="picture-map-nearest-address"></p>
-			<?= $form->field($model,'loc_formatted_addr')->textInput(['id'=>'picture-map-loc-formatted-addr', ]) ?>
-			<!-- Hidden fields are not reset upon a form reset; therefore, we need to use normal fields which we hide -->
-			<?= Html::activeInput('text', $model,'loc_lat', ['id'=>'picture-map-loc-lat', 'style' => 'display:none', ]) ?>
-			<?= Html::activeInput('text', $model,'loc_lng',['id'=>'picture-map-loc-lng', 'style' => 'display:none', ]) ?>
-			<?= Html::activeHiddenInput($model,'org_loc_lat', ['id'=>'picture-map-loc-lat-org', ]) ?>
-			<?= Html::activeHiddenInput($model,'org_loc_lng',['id'=>'picture-map-loc-lng-org', ]) ?>
+		<?php echo Html::label('Kartensuche (via Google Maps)','picture-map-search-address'); ?>
+		<input type="text" id="picture-map-search-address" class="form-control" autocomplete="off" value="<?=$model->loc_formatted_addr?>" placeholder="Kartensuche">
 		</div>
+		<div class ="form-group">
+		<?php echo Html::label('Nächstliegende Adresse (ermittelt via Google Maps)','picture-map-nearest-address'); ?>
+		<p id="picture-map-nearest-address"></p>
+		</div>
+		<?= $form->field($model,'loc_formatted_addr')->textInput(['id'=>'picture-map-loc-formatted-addr', ]) ?>
+		<!-- Hidden fields are not reset upon a form reset; therefore, we need to use normal fields which we hide -->
+		<?= Html::activeInput('text', $model,'loc_lat', ['id'=>'picture-map-loc-lat', 'style' => 'display:none', ]) ?>
+		<?= Html::activeInput('text', $model,'loc_lng',['id'=>'picture-map-loc-lng', 'style' => 'display:none', ]) ?>
+		<?= Html::activeHiddenInput($model,'org_loc_lat', ['id'=>'picture-map-loc-lat-org', ]) ?>
+		<?= Html::activeHiddenInput($model,'org_loc_lng',['id'=>'picture-map-loc-lng-org', ]) ?>
 	</div>
 	<div class="col-lg-4">
 		<!-- The Google maps canvas needs absolute coordinates -->
