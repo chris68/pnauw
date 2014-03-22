570bbeb2ba584fc04e8b8322b1bd35fbd5847177
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2014-03-22 13:18:02 +0100

    PHPDocs: "app\models" changed to "common\models"
    (cherry picked from commit 73fa5137d70421e21d8971e048c2903be2e69218)
    
    Conflicts:
    
    	frontend/views/site/login.php

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -1,7 +1,7 @@
 <?php
 /* @var $this yii\web\View */
 /* @var $form yii\widgets\ActiveForm */
-/* @var $model app\models\LoginForm */
+/* @var $model common\models\LoginForm */
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
