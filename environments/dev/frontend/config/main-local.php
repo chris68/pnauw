b777a6610d0d6fa8320c1522975d4f0c4a7b60de
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2014-10-16 22:53:40 +0200

    Update to yii2 2.0.0: Provide the feasibility to use debug also on remote (via allowedIPs)

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -12,8 +12,10 @@ $config = [
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
-
+    $config['modules']['debug'] = [
+		'class' => 'yii\debug\Module',
+        // 'allowedIPs' => ['*.*.*.*', ], // uncomment for debugging on a remote host!
+ 	];
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = 'yii\gii\Module';
 }
