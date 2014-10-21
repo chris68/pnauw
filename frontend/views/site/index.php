56db9921f96245e123c21425deda197c40ca9197
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2014-10-21 10:29:06 +0200

    Better intro page concerning guest access (resolves #77)

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -25,24 +25,36 @@ $this->title = \Yii::$app->name;
 				<p>Sie sind ein Autofahrer und hatten einen Zettel am Auto?</p>
 				<p>Dann schauen Sie doch einfach, was man zu Ihrem Parkverhalten sagt!</p>
 
-				<p><a class="btn btn-default" href="<?= Url::to(['/picture/index', 's[time_range]' => '-30;0', 's[map_bind]' => '1'])?>">Die aktuellen Vorfälle&raquo;</a></p>
+				<p>
+					<a class="btn btn-default" href="<?= Url::to(['/picture/index', 's[time_range]' => '-30;0', 's[map_bind]' => '1'])?>">Die aktuellen Vorfälle&raquo;</a>
+				</p>
 			</div>
 			<div class="col-md-4">
 				<h2>Fußgänger</h2>
 
 				<p>Sie sind Fußgänger und finden es nicht lustig, wie ihre Gehwege zugeparkt sind?</p> 
-				<p>Dann melden Sie die Vorfälle doch einfach. Geht ganz schnell mit der Handykamera und Sie müssen sich dafür nicht einmal registrieren!</p>
+				<p>Dann melden Sie die Vorfälle doch einfach!</p>
 
-				<p><a class="btn btn-default" href="<?= Url::to(['/picture/guestcapture'])?>">Vorfall als Gast melden&raquo;</a></p>
+				<p>
+					<a class="btn btn-default" href="<?= Url::to(['/picture/capture'])?>">Vorfall melden&raquo;</a>
+					<a class="btn btn-default" href="<?= Url::to(['/picture/guestcapture'])?>">als Gast&raquo;</a>
+				</p>
+
+				<p>Geht ganz schnell mit der Handykamera und mit dem <a href="<?=Url::to(['help']).'#picture-guestcapture'?>" target="_blank">Gastzugang</a>  müssen sich nicht einmal registrieren.</p>
 			</div>
 			<div class="col-md-4">
 				<h2>Mitarbeit</h2>
 
-				<p>Sie wollen richtig mitarbeiten und regelmäßig oder im größeren Stil Gehwegparker dokumentieren?</p>
-				<p>Dann <?= Html::a("registrieren",['/site/signup'])?> Sie sich und laden ihre ganzen Bilder von Gehwegparkern hoch!</p>
+				<p>Sie wollen im größeren Stil Gehwegparker dokumentieren?</p>
+				<p>Dann laden Sie einfach ihre ganzen Bilder hoch!</p>
 
-				<p><a class="btn btn-default" href="<?= Url::to(['picture/upload'])?>">Bilder hochladen&raquo;</a></p>
-				<p>Und wenn Sie länger dabei sind, dann beantragen Sie am besten schleunigst eine höhere <a href="<?=Url::to(['help']).'#user-level'?>">Berechtigungsstufe</a></p>
+				<p>
+					<a class="btn btn-default" href="<?= Url::to(['picture/upload'])?>">Bilder hochladen&raquo;</a>
+					<a class="btn btn-default" href="<?= Url::to(['picture/guestupload'])?>">als Gast&raquo;</a>
+				</p>
+				
+				<p>Geht auch im <a href="<?=Url::to(['help']).'#picture-guestupload'?>" target="_blank">Gastzugang</a>, aber besser Sie <?= Html::a("registrieren",['/site/signup'])?> sich hierzu.
+				Und bei reger Teilnahme sollten Sie baldigst eine höhere <a href="<?=Url::to(['help']).'#user-level'?>" target="_blank">Berechtigungsstufe</a> beantragen.</p>
 			</div>
 		</div>
 
