4d4d0f9c109f48c4fcb8a13192753ab5d04f7b7d
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2014-11-15 22:01:03 +0100

    Explicitly set the mailer also for prod

--- environments/prod/frontend/config/main-local.php
+++ environments/prod/frontend/config/main-local.php
@@ -5,5 +5,9 @@ return [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
     ],
 ];
