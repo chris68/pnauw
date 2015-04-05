eda523fb1018143d46183866b033f8f972710b7b
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2015-04-05 22:06:34 +0200

    Prepare for release V1.3.0

--- common/config/main.php
+++ common/config/main.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'name' => 'Parke nicht auf unseren Wegen',
-    'version' => '1.2.2',
+    'version' => '1.3',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
--- common/mail/notification-release.php
+++ common/mail/notification-release.php
@@ -9,6 +9,10 @@ Hallo <?=$user->username?>!
 
 Bei der Webplattform parke-nicht-auf-unseren-wegen.de ist soeben die neue Release <?=Yii::$app->version?> veröffentlicht worden. Die Neuerungen finden Sie in der Releasehistorie (<?=Yii::$app->urlManager->createAbsoluteUrl(['site/releasehistory'])?>).
 
+Es würde uns freuen, wenn wir Sie bald mal wieder auf unserer Plattform begrüßen dürften.
+
 ===================================================================================================
 
 Sie bekommen diese Email, weil Sie bei parke-nicht-auf-unseren-wegen.de unter dem Nutzer <?=$user->username?> mit der Emailadresse <?=$user->email?> registriert sind. Wenn Sie parke-nicht-auf-unseren-wegen.de nicht mehr nutzen wollen, dann löschen Ihren Nutzer bitte in der Nutzerverwaltung.
+
+Bitte antworten Sie nicht auf diese Mail, sondern nutzen Sie stattdessen bitte die Kontaktseite (<?=Yii::$app->urlManager->createAbsoluteUrl(['site/contact'])?>)
