42c6a675f956af761f5a84be1a8f0725ab0c2235
chris68 <chris68@users.noreply.github.com>
2015-04-05 22:30:35 +0200

    Set release date

--- frontend/views/site/releasehistory.php
+++ frontend/views/site/releasehistory.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
         Die Plattform wird laufend weiterentwickelt und damit Sie schnell erkennen können, wann was neu hinzugekommen ist, haben wir hier die Releasehistorie veröffentlicht. Kleinere Änderungsrelease mit Fehlerkorrekturen werden hier jedoch nicht aufgeführt.
     </p>
     <h3>
-        Version 1.3 (??.04.2015)
+        Version 1.3 (05.04.2015)
     </h3>
     <p>
         
