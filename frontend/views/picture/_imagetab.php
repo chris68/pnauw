aa1a36db370ba4813715b3842add1bfc5e39e95a
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2016-12-28 14:22:16 +0100

    Acquire location in high accuracy and set to higher zoom level

--- frontend/views/picture/_imagetab.php
+++ frontend/views/picture/_imagetab.php
@@ -119,7 +119,7 @@ use \yii\helpers\Html;
                     }
                 );
 
-                map.locate({setView: true, watch: true, maxZoom: 16});
+                map.locate({setView: true, watch: true, enableHighAccuracy: true, maxZoom: 17});
                 var positionLayerGroup = L.layerGroup([]);
                 positionLayerGroup.addTo(map);
 
