2f7122ff6fc1a529f7382bea57107686d45766eb
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2016-12-29 23:17:36 +0100

    Owner Scope in widget not honored

--- frontend/widgets/VehicleIncidentHistory.php
+++ frontend/widgets/VehicleIncidentHistory.php
@@ -28,7 +28,7 @@ class VehicleIncidentHistory extends \yii\base\Widget
         parent::init();
 
         if (!empty($this->picture->vehicle_reg_plate)) {
-            $count = Picture::find()->where(['vehicle_reg_plate' => $this->picture->vehicle_reg_plate])->ownerScope()->count();
+            $count = Picture::find()->ownerScope()->andWhere(['vehicle_reg_plate' => $this->picture->vehicle_reg_plate])->count();
 
             if ($count > 3) {
                 $content = 'Bereits <b style="color:red">'.$count.' Vorfälle</b>';
