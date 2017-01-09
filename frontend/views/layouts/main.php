cd199abde35849bab7674d97549f2b211a938304
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2017-01-09 20:56:37 +0100

    favicon must be under root

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -50,7 +50,7 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Plattform zum Dokumentieren von missbräuchlichen Parken auf Gehwegen, Radwegen und in verkehrsberuhigten Zonen">
     <?= Html::csrfMetaTags() ?>
-    <link rel="icon" type="image/x-icon" href="<?=Url::to('favicon.ico')?>">
+    <link rel="icon" type="image/x-icon" href="<?=Url::to('/favicon.ico')?>">
     <title><?= $this->title ?></title>
     <?php $this->head() ?>
 </head>
