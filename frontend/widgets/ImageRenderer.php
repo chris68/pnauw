bb36cfc5eac5320cd3b0f9bdab64224ec58074fb
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2017-01-16 19:19:48 +0100

    Nice pnauw pic instead of the generated one

--- frontend/widgets/ImageRenderer.php
+++ frontend/widgets/ImageRenderer.php
@@ -34,41 +34,13 @@ class ImageRenderer extends \yii\base\Widget
             $this->options['alt'] = 'Bild des Vorfalls';
         } else {
             // If no image is available construct one!
-            $image = new \Imagick();
-            $draw = new \ImagickDraw();
-            $draw->setFont('AvantGarde-BookOblique');
-            switch ($this->size) {
-                // According to: http://flickrj.sourceforge.net/api/com/aetrion/flickr/photos/Size.html
-                case 'thumbnail':
-                    $image->newImage(75, 100, new \ImagickPixel('gray'));
-                    $draw->setFontSize(11);
-                    $offset = 20;
-                    break;
-                case 'small':
-                    $image->newImage(180, 240, new \ImagickPixel('gray'));
-                    $draw->setFontSize(30);
-                    $offset = 60;
-                    break;
-                case 'medium':
-                    $image->newImage(375, 500, new \ImagickPixel('gray'));
-                    $draw->setFontSize(65);
-                    $offset = 120;
-                    break;
-                case 'large':
-                    $image->newImage(768, 1024, new \ImagickPixel('gray'));
-                    $draw->setFontSize(130);
-                    $offset = 250;
-                    break;
-            }
-
-
-            /* Create text */
-            $image->annotateImage($draw, 10, $offset, 0, "Ohne Bild\n\n   oder\n\nBild noch\nim Review");
+            $image = new \Imagick(\Yii::getAlias('@webroot').'/img/pnauw.jpeg');
+            $image->scaleimage(0, 200);
             $image->setImageFormat('jpg');
             $rawdata = bin2hex($image->getimageblob());
             
             $this->options['src'] = 'data:image/jpg;base64,' . base64_encode(hex2bin($rawdata));
-            $this->options['alt'] = 'Bild des Vorfalls';
+            $this->options['alt'] = 'Standardbild, weil kein Bild des Vorfalls vorhanden oder dies noch Genehmigung ist';
         }
     
         echo \yii\helpers\Html::tag('img','',$this->options);
