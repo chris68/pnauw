bdda5ae9b7be939c01148a956892b1d48b7c47df
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2017-02-12 22:14:22 +0100

    Prepare for release 2.2.1

--- common/config/main.php
+++ common/config/main.php
@@ -2,7 +2,7 @@
 return [
 // @chris68
     'name' => 'Parke nicht auf unseren Wegen',
-    'version' => '2.2.0',
+    'version' => '2.2.1',
 
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
