732a6c919da6d72fbe3f08ab99adcbc89fce5346
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2019-01-27 15:28:41 +0100

    Release letter with terms and privacy changes

--- common/mail/notification-release.php
+++ common/mail/notification-release.php
@@ -9,6 +9,8 @@ Hallo <?=$user->username?>!
 
 Bei der Webplattform parke-nicht-auf-unseren-wegen.de ist soeben die neue Release <?=Yii::$app->version?> veröffentlicht worden. Die Neuerungen finden Sie in der Releasehistorie (https://parke-nicht-auf-unseren-wegen.de/site/releasehistory).
 
+Eventuell wurden auch die Nutzungsbedingungen (https://parke-nicht-auf-unseren-wegen.de/site/terms) oder Datenschutzbedingungen (https://parke-nicht-auf-unseren-wegen.de/site/privacy) geändert. Am besten lesen Sie sich diese daher noch einmal schnell durch, ob Sie immer noch mit allem einverstanden sind.
+
 Es würde uns freuen, wenn wir Sie bald mal wieder auf unserer Plattform begrüßen dürften.
 
 ===================================================================================================
