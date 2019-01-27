cfa41a2b0d02eac95f9a2d75cdf338365269aaf0
Christoph Toussaint <christoph.toussaint@mailwitch.com>
2019-01-27 13:37:43 +0100

    Wrong params files

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -2,4 +2,5 @@
 return [
 // @chris68
     // 'adminEmail' => 'admin@example.com', // adminEmail already set in common
+    'alpr.secretKey' => $oauth['alpr']['secretKey'], // Dirty hack; $oauth is loaded in main.php
 ];
