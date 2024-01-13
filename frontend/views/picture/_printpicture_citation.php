4ce94530d598f82e0a59e1eb907e49cb08e81367
chris68 <chris68@users.noreply.github.com>
2024-01-13 15:11:29 +0100

    Time only hour and braces for hash

--- frontend/views/picture/_printpicture_citation.php
+++ frontend/views/picture/_printpicture_citation.php
@@ -16,7 +16,7 @@ PictureLocationmapAsset::register($this);
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
     
     <h1>
-        <?=Html::encode(($printParameters->visibility=='unchanged'?$model->vehicle_reg_plate:substr(hash('md5',$model->vehicle_reg_plate),0,8)).' / '.date_format(date_create($model->taken),'d.m.Y').' / #'.$model->id)?>
+        <?=Html::encode(($printParameters->visibility=='unchanged'?$model->vehicle_reg_plate:'{'.substr(hash('md5',$model->vehicle_reg_plate),0,8).'}').' / '.date_format(date_create($model->taken),'d.m.Y').' / #'.$model->id)?>
         <?=((yii::$app->user->can('isObjectOwner', array('model' => $model)))?('&nbsp;'.Html::a('<span class="glyphicon glyphicon-pencil"></span>', ['picture/update','id'=>$model->id], ['class' => 'delete-before-printing', 'target' => '_blank'])):'')?>
             
     </h1>
@@ -27,7 +27,10 @@ PictureLocationmapAsset::register($this);
             [
                 'model' => $model,
                 'attributes' => [
-                    'taken',
+                    [
+                        'label' => 'Aufgenommen am',
+                        'value' => $printParameters->visibility=='unchanged'?'taken':date_format(date_create($model->taken),'d.m.Y h'),
+                    ],
                     [
                         'label' => 'Kennzeichen',
                         'value' => $printParameters->visibility=='unchanged'?$model->vehicle_reg_plate:'',
