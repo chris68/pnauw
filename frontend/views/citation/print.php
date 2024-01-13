864e971e3e49dc50cf64b6d57e5acc615ba0779d
chris68 <chris68@users.noreply.github.com>
2024-01-13 16:03:54 +0100

    Redact with black background

--- frontend/views/citation/print.php
+++ frontend/views/citation/print.php
@@ -74,7 +74,7 @@ $printParameters->load(Yii::$app->request->get());
     </p>
     <h2>Spezifische Angaben für die Beschwerde</h2>
     <?php endif; ?>
-    <?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->description:preg_replace('/`.*`/i','(Passage wurde verschleiert)',$model->description)))?> 
+    <?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->description:preg_replace('/`.*`/i','<span style="background-color:black;">          </span>',$model->description)))?> 
     <div style="page-break-before: always;"></div>
     
     <?php endif; ?>
@@ -126,7 +126,7 @@ $printParameters->load(Yii::$app->request->get());
         oder das Parkverhalten nicht tolerierbar ist, auch anzeigen. Was hiermit gerade geschieht.
     </p>
     <h2>Zeuge und weitere spezifische Angaben für die Anzeige</h2>
-    <p><?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->description:preg_replace('/`.*`/i','&#35;&#35;&#35;&#35;&#35;',$model->description)))?></p>
+    <p><?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->description:preg_replace('/`.*`/i','<span style="background-color:black;">          </span>',$model->description)))?></p>
     <h2>Generelle Erläuterungen</h2>
     <p>
         Unter Vorfall ist genau dokumentiert, wie der Anzeiger die Lage entschätzt. Wenn es dort heißt <b>Gehwegparken (mit Behinderung)</b>, 
@@ -170,6 +170,6 @@ $printParameters->load(Yii::$app->request->get());
     <?php if ($printParameters->history == 'show'): ?>
     <div style="page-break-before: always;"></div>
     <h2>Verlaufshistorie</h2>
-    <?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->history:preg_replace('/`.*`/i','&#35;&#35;&#35;&#35;&#35;',$model->history)))?> 
+    <?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->history:preg_replace('/`.*`/i','<span style="background-color:black;">          </span>',$model->history)))?> 
     <?php endif ?>
 </div>
