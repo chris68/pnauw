02e23201a9093011b6649ac462c079cd00195048
chris68 <chris68@users.noreply.github.com>
2024-01-13 14:22:22 +0100

    Hide platform and ` for redacting

--- frontend/views/citation/print.php
+++ frontend/views/citation/print.php
@@ -66,7 +66,7 @@ $printParameters->load(Yii::$app->request->get());
     <h1><?= $this->title ?></h1>
     <?php if ($model->type == 'complaint') : ?>
     <p>
-        Dies ist eine informelle Beschwerde (<b>keine Anzeige!</b>), die über die Plattform <b>Parke-nicht-auf-unseren-Wegen.de</b> erstellt wurde. Mit dieser Plattform 
+        Dies ist eine informelle Beschwerde (<b>keine Anzeige!</b>), die mit einem speziellen System erstellt wurde. Mit solchen Systemen 
         können betroffene Bürger Gehwegparker dokumentieren, die Autofahrer auf ihr missbräuchliches Parken hinweisen und das Fehlverhalten an die entsprechenden Behörden melden. Was hiermit gerade geschieht.
     </p>
     <p>
@@ -74,7 +74,7 @@ $printParameters->load(Yii::$app->request->get());
     </p>
     <h2>Spezifische Angaben für die Beschwerde</h2>
     <?php endif; ?>
-    <?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->description:preg_replace('/\*.*\*/i','(Passage wurde verschleiert)',$model->description)))?> 
+    <?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->description:preg_replace('/`.*`/i','(Passage wurde verschleiert)',$model->description)))?> 
     <div style="page-break-before: always;"></div>
     
     <?php endif; ?>
@@ -121,12 +121,12 @@ $printParameters->load(Yii::$app->request->get());
     <?php if ($model->type == 'citation') : ?>
     <h1><?= $this->title ?></h1>
     <p>
-        Dies ist eine Privatanzeige, die über die Plattform <b>Parke-nicht-auf-unseren-Wegen.de</b> erstellt wurde. Mit dieser Plattform 
+        Dies ist eine Privatanzeige, die mit einem speziellen System erstellt wurde. Mit solchen Systemen 
         können betroffene Bürger Gehwegparker dokumentieren, die Autofahrer auf ihr missbräuchliches Parken hinweisen und, wenn es nicht hilft
         oder das Parkverhalten nicht tolerierbar ist, auch anzeigen. Was hiermit gerade geschieht.
     </p>
     <h2>Zeuge und weitere spezifische Angaben für die Anzeige</h2>
-    <p><?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->description:preg_replace('/\*.*\*/i','&#35;&#35;&#35;&#35;&#35;',$model->description)))?></p>
+    <p><?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->description:preg_replace('/`.*`/i','&#35;&#35;&#35;&#35;&#35;',$model->description)))?></p>
     <h2>Generelle Erläuterungen</h2>
     <p>
         Unter Vorfall ist genau dokumentiert, wie der Anzeiger die Lage entschätzt. Wenn es dort heißt <b>Gehwegparken (mit Behinderung)</b>, 
@@ -145,7 +145,7 @@ $printParameters->load(Yii::$app->request->get());
         Dies gilt in der Regel (d.h. wenn dem nicht indiviudell und ausdrücklich wiedersprochen wird) weniger bei Nichtumsetzen von vereinzelten Anzeigen/Vorfällen - 
         hier soll in der Regel aus Effizienzgründen auf die Korrektheit der Entscheidung vertraut werden. 
         Sondern es geht vielmehr darum, dass ein Nichtumsetzen <b>aller oder sehr vieler Anzeigen</b> rechtlich eher nicht durch das Opportunitätsprinzip
-        abgedeckt wäre. Und dann will der Anzeiger eventuell die Möglichkeit des Rechtsweges oder der Dienstaufsichtsbeschwerde einschlagen.
+        abgedeckt wäre. Und dann will der Anzeiger eventuell die Möglichkeit des Rechtsweges oder der Dienst- oder Fachaufsichtsbeschwerde einschlagen.
         Und daher muss sichergestellt sein, dass die Anzeigen nicht einfach und in großen Stil ohne weitere Benachrichtigung eingestellt werden.
         Denn der Anzeiger macht die Sache ja meist nicht aus Spass, sondern eher aus Notwehr, weil es die offiziellen Stellen nicht machen!
     </p>
@@ -170,6 +170,6 @@ $printParameters->load(Yii::$app->request->get());
     <?php if ($printParameters->overviewlist == 'show'): ?>
     <div style="page-break-before: always;"></div>
     <h2>Verlaufshistorie</h2>
-    <?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->history:preg_replace('/\*.*\*/i','&#35;&#35;&#35;&#35;&#35;',$model->history)))?> 
+    <?=HtmlPurifier::process(Markdown::convert($printParameters->visibility=='unchanged'?$model->history:preg_replace('/`.*`/i','&#35;&#35;&#35;&#35;&#35;',$model->history)))?> 
     <?php endif ?>
 </div>
